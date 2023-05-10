195d57f569089549d5d62bfcfd454beb86ce931c
devbrunosanches <dev.brunosanches@gmail.com>
2023-05-10 16:03:50 -0300

    chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from 'react'
+import {SubHeading} from "../../components"
+import {images} from "../../constants"
 
-import './Chef.css';
+import './Chef.css'
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef img" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word"/>
+      <h1 className="headtext__cormorant">
+        What We Believe In
+      </h1>
+      <div className="app__chef-content">
+        <div className="app__chef_content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Amet, ratione vitae voluptate id nisi minus, perferendis facere accusantium dolores ab architecto! Nostrum unde minus obcaecati modi reiciendis quasi enim placeat.
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Simbato Yamasaki</p>
+        <p className="p__opensans">
+          Chef & Founder
+        </p>
+        <img src={images.sign} alt="chef sign"/>
+      </div>
+    </div>
   </div>
-);
+)
 
-export default Chef;
+export default Chef
